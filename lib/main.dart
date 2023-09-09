c6ac29f542b88784a67b8b0f0a6020026139e106
mayanksalescode <mayank.aggarwal@salescode.ai>
2023-09-09 11:33:15 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Myapp());
+}
+
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("welcome to 30 days ")
+            )
+            ),
+
+ 
+        ) 
+
+    );
+  }
+}
